fbab7d53f0f2e120ee16f4def8e479dc6e157428
Kiruthiga Kanagalingam <kiruanime2003@gmail.com>
2022-08-22 21:15:06 +0530

    added js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    var number = document.querySelector("p");
+    var modifyNumber = Array.from(document.getElementsByTagName("button"));
+    var counter=0;
+
+    modifyNumber[0].addEventListener("click", function(){
+        counter++;
+        number.innerHTML = counter;
+    });
+
+    modifyNumber[1].addEventListener("click", function(){
+        if(counter==0) counter=0;
+        else{
+            counter--;
+            number.innerHTML = counter;
+        }
+    });
+
+    modifyNumber[2].addEventListener("click", function(){
+        counter=0;
+        number.innerHTML = counter;
+    });
 });
